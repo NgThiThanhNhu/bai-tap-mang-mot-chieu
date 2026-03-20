6d8e84e3dbe20e4a5dc9e45b2c71d422bfe74559
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-03-20 10:17:58 +0700

    them bai 3

---
+++ Bai4/Bai4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Đếm số lần xuất hiện của từng phần tử trong mảng
+Console.Write("Nhap so phan tu cua mang: ");
+int n = int.Parse(Console.ReadLine());
+int[] mang = new int[n];
+
+for (int i=0; i<n; i++)
+{
+    Console.Write($"Nhap phan tu {i}: ");
+    mang[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i=0; i<n; i++)
+{
+    int count = 0;
+    for (int j=0; j<n; j++)
+    {
+        if (mang[i] == mang[j] )
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"So lan xuat hien cua so {mang[i]} la {count} lan");
+}
