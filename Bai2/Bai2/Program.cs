318cb4fdb7b104c4a88baa9192f8deba1e8da35d
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-03-20 08:22:32 +0700

    them bai tap 2

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,33 @@
+﻿//tim phan tu duy nhat trong mang
+Console.Write("Nhap so phan tu cua mang: ");
+int n = int.Parse(Console.ReadLine());
+int[] mang = new int[n];
+for (int i=0; i<n; i++)
+{
+    Console.Write($"phan tu {i}: ");
+    mang[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Phan tu duy nhat trong mang: ");
+for (int i=0; i<n; i++)
+{
+    int count = 0;
+    for (int j=i+1; j<n; j++)
+    {
+        if (mang[i] == mang[j])
+        {
+            count++;
+        }
+    }
+    for (int k = i + 1; k < n; k++)
+    {
+        if (mang[i] == mang[k])
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        Console.Write("{0} ", mang[i]);
+    }
+}
